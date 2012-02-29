1d04a3cb38f9f41c2038ac9cdbb5f511151669b7
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-02-29 06:18:35 +0530

    Handle hooks later. Let's just do integer lists for now

--- defs.rkt
+++ defs.rkt
@@ -4,9 +4,16 @@
 
 ;; A complex sort (e.g. List) has data about the base sort, a creator function
 ;; (which takes the base sort and a list of sort parameters to apply and produces
-;; an immutable datatype-instance. We also want to cache values for specific sort
-;; parameters. 
+;; an immutable datatype-instance. We also want to cache instances for specific sort
+;; parameters (so (List Int) followed by (List Int) should return the same
+;; datatype-instance.
 (struct z3-complex-sort (base-sort creator instance-hash))
+
+;; Creates a new complex sort. This adds hooks for each constructor to the namespace
+;; provided.
+;; XXX handle hooks
+(define (make-complex-sort base-sort creator ns hook-ids)
+  (let ([res (z3-complex-sort base-sort creator (make-hash))])))
 
 ;; Given a base sort and parameter sorts get or create a parameterized datatype.
 (define (get-or-create-instance sort params)
