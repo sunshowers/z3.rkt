51329cbe46661141b8d43f26d8202e67e562a8c6
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-03-16 03:30:10 +0530

    Move to text-based tests because they're faster to run

--- run-tests.rkt
+++ run-tests.rkt
@@ -1,8 +1,11 @@
 #lang racket
 
 (require rackunit rackunit/text-ui rackunit/gui)
-; Load all files in the directory
 (require "tests/test-booleans.rkt"
          "tests/test-integers.rkt")
 
-((make-gui-runner) test-booleans test-integers)
+(run-tests
+ (test-suite
+  "Z3 API tests"
+  test-booleans
+  test-integers))
