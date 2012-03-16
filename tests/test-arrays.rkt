9cd657d55a64134859b967bd345642eae789a59a
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-03-17 01:07:31 +0530

    Add a couple more tests to check what is going on

--- tests/test-arrays.rkt
+++ tests/test-arrays.rkt
@@ -11,4 +11,6 @@
     (smt:declare-fun a () (Array Int Int))
     (smt:assert (= (select a 0) 5))
     (smt:assert (< (select a 1) (select a 0)))
-    (check-eq? (smt:check-sat) 'sat))))
+    (check-eq? (smt:check-sat) 'sat)
+    (check-eq? (smt:eval (select a 0)) 5)
+    (check-true (< (smt:eval (select a 1)) (smt:eval (select a 0)))))))
