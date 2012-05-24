1dd655f61c70b31df1d33ac232c693a28f2fb8b9
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-05-24 15:15:50 +0530

    Add a test case for append to test-list-helpers.rkt

--- tests/test-list-helpers.rkt
+++ tests/test-list-helpers.rkt
@@ -19,4 +19,20 @@
     (check-eq? (smt:check-sat) 'sat)
     (check-equal? (z3-list->list (smt:eval v1)) '())
     (check-equal? (z3-list->list (smt:eval v2)) '(5 4 3 2 1))
-    (check-equal? (z3-list->list (smt:eval v3)) '(10 9 8 7 6 5 4 3 2 1)))))
+    (check-equal? (z3-list->list (smt:eval v3)) '(10 9 8 7 6 5 4 3 2 1))))
+  
+  (test-case
+   "Test append"
+   (smt:with-context
+    (smt:new-context-info)
+    (define append (make-append 10))
+    (smt:declare-fun v1 () IntList)
+    (smt:assert (=/s v1 (append (nil/s) (nil/s))))
+    (smt:declare-fun v2 () IntList)
+    (smt:assert (=/s v2 (append (list->z3-list '(1 2 3)) (list->z3-list '(4 5 6)))))
+    (smt:declare-fun v3 () IntList)
+    (smt:assert (=/s v3 (append (list->z3-list '(1 2 3 4 5 6 7 8)) (list->z3-list '(9 10 11 12)))))
+    (check-eq? (smt:check-sat) 'sat)
+    (check-equal? (z3-list->list (smt:eval v1)) '())
+    (check-equal? (z3-list->list (smt:eval v2)) '(1 2 3 4 5 6))
+    (check-equal? (z3-list->list (smt:eval v3)) '(1 2 3 4 5 6 7 8 9 10 11 12)))))
